2f1efdbcb358f3be8770d9d2bbad6e5867cf67f7
Oluwafemi  Babayemi <femi.gabs@gmail.com>
2022-03-17 08:40:20 +0100

    code setup

---
+++ app/config/app.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import morgan from 'morgan';
+import { json, urlencoded } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import apiV1Routes from '../routes/v1';
+import config from './env';
+import { Helper, genericErrors, constants } from '../utils';
+
+const { errorResponse, successResponse } = Helper;
+const { notFoundApi } = genericErrors;
+const {
+  WELCOME,
+  v1,
+  LANNISTER_RUNNING
+} = constants;
+
+const appConfig = (app) => {
+  app.use(morgan('combined', { stream: logger.stream }));
+  app.use(helmet());
+  app.use(json());
+  app.use(urlencoded({ extended: true }));
+  app.use(cors());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers',
+      'Authorization, Origin, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+  app.get('/', (req, res) => successResponse(res, { message: WELCOME }));
+  app.use(v1, apiV1Routes);
+  app.use((req, res, next) => {
+    next(notFoundApi);
+  });
+  app.use((err, req, res, next) => errorResponse(req, res, err));
+  const port = config.PORT || 3100;
+  app.listen(port, () => {
+    logger.info(`${LANNISTER_RUNNING} ${port}`);
+  });
+};
+
+export default appConfig;
---
+++ app/config/env/test.js
@@ -0,0 +1,5 @@
+import 'dotenv/config';
+
+export default {
+  DATABASE_URL: process.env.LANNISTER_TEST_URL
+};
